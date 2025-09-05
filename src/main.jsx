8b9d0dbf08126d562e6e43c3d34aca7cbedd1b20
Ujaval Savaliya <ujavalsavaliya@gmail.com>
2025-09-05 19:20:31 +0530

    Update main.jsx
    change file name

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import ProtectedRoute from "./Component/ProtectedRoute.jsx";
 import "./index.css";
 import {
   Route,
